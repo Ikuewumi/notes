4002f40b248fcd4a2e81a66fb37185c8d169455e
Ayobami <ayobamsik@gmail.com>
2023-02-08 21:23:41 +0100

    header responsive patch

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,17 +3,17 @@
 
 		<div>
 
-			<div class="mx-auto grid grid-cols-3 w-full justify-between place-content-center place-items-center pr-1 py-2">
+			<div class="mx-auto grid grid-cols-2 md:grid-cols-3 w-full justify-between  place-items-center pr-1 py-2">
 
 
 
 				<!-- Logo -->
 				<span class="flex flex-row place-items-center gap-x-2 justify-self-start">
-					<svg class="w-[65px] aspect-square fill-[#718093]" view-box="0 0 30 30">
+					<svg class="w-[45px] md:w-[65px] aspect-square fill-[#718093]" view-box="0 0 30 30">
 						<use href="#outlines"></use>
 					</svg>
 
-					<strong class="font-header font-semibold text-4xl">notes</strong>
+					<strong class="font-header font-semibold text-3xl md:text-4xl">notes</strong>
 				</span>
 
 
@@ -27,7 +27,7 @@
 
 
 				<button
-					class="justify-self-end font-header px-6 py-2 bg-slate-600 hover:bg-slate-500 text-white rounded-sm">Sign
+					class="justify-self-end ml-auto font-header px-6 py-2 bg-slate-600 hover:bg-slate-500 text-white rounded-sm">Sign
 					Up</button>
 
 
