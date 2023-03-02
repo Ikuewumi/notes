7f1d09e067d9f394a82b9cb85bd5d834fd465ec1
Ayobami Ikuewumi <ayobamsik@gmail.com>
2023-03-03 00:17:19 +0100

    fix image typing

--- src/composables/z.ts
+++ src/composables/z.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const zMetadata = z.object({
 	title: z.string().min(1),
-	image: z.string().optional(),
+	image: z.string().default(""),
 	tags: z.string().min(1),
 	categories: z.string().min(1),
 	excerpt: z.string().min(30)
